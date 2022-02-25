ad1beb4050c5541f6b6de741032a78916b492c86
yagamiar15 <dario11elme@hotmail.com>
2022-02-24 23:38:48 -0300

    Añadida NavBar

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar";
+
+const Header=()=>{
+    return (
+       <header>
+           <NavBar/>
+       </header>
+    )
+}
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const NavBar=()=>{
+    return (
+        <nav>
+        <h3>
+            Tienda Pacheco
+        </h3>
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+                <a href="#">Productos</a>
+                <a href="#">Contacto</a>
+                </li>
+        </ul>
+        </nav>
+    )
+}
+export default NavBar;
