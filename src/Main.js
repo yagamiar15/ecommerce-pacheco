44a48f272cade3d7c8e43d53cab3a6c48dd3d82d
yagamiar15 <dario11elme@hotmail.com>
2022-03-03 23:38:16 -0300

    añadida botonera con estilos

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
 import ItemListContainer from "./components/ItemListContainer";
+import ItemCount from "./components/ItemCount";
+
 
 const Main = () => {
   return (
     <main className="main">
       <ItemListContainer user="Usuario" />
+      <ItemCount stock={5} inicial={1} />
     </main>
   );
 };
